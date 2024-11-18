cba3a681161ec6832ae8f57e823ac6255c3d5e02
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-11-18 14:19:52 +0100

    :white_check_mark: Konfigurasjon for tester
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>

---
+++ app/Meldekort.test.tsx
@@ -0,0 +1,5 @@
+import {expect, test} from "vitest";
+
+test('hello pello', () => {
+    expect(true).toBeTruthy();
+});
---
+++ vitestSetup.ts
@@ -0,0 +1,16 @@
+import {afterEach, beforeAll, vi} from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import {cleanup} from '@testing-library/react';
+
+beforeAll(() => {
+  vi.mock('next/navigation', () => ({
+    useParams: vi
+        .fn()
+        .mockReturnValue({behandlingsreferanse: '123', aktivtSteg: 'HEIHEI'}),
+    useRouter: vi.fn()
+  }))
+})
+
+afterEach(() => {
+  cleanup();
+});
