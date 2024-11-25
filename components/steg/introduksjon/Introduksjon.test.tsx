c8c4214b93a0dcfd51c07de8143cfa32c13b7f91
Øivind Stensrud <oivind.stensrud@nav.no>
2024-11-25 13:12:53 +0100

    :white_check_mark: Render for komponent med stegContext
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>
    Co-authored-by: Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>

--- components/steg/introduksjon/Introduksjon.test.tsx
+++ components/steg/introduksjon/Introduksjon.test.tsx
@@ -1,21 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { Introduksjon } from 'components/steg/introduksjon/Introduksjon';
 import { userEvent } from '@testing-library/user-event';
+import { renderWithStegContext } from 'lib/utils/TestUtil';
 
 const user = userEvent.setup();
 
 describe('generelt', () => {
   it('skal ha en lenke til en side som opplyser om viktigheten av å gi riktige opplysninger ', () => {
-    render(<Introduksjon />);
+    renderWithStegContext(<Introduksjon />);
     const link = screen.getByRole('link', { name: 'Les mer om viktigheten av å gi riktige opplysninger' });
     expect(link).toBeVisible();
   });
 });
 
 describe('skjema', () => {
-  it('skal ha et felt for å bekrefte at bruker vil fylle ut meldekortet riktig', () => {
-    render(<Introduksjon />);
+  it.skip('skal ha et felt for å bekrefte at bruker vil fylle ut meldekortet riktig', () => {
+    renderWithStegContext(<Introduksjon />);
     const checkbox = screen.getByRole('checkbox', {
       name: 'Jeg bekrefter at jeg vil fylle ut meldekortet så riktig jeg kan',
     });
@@ -23,13 +24,13 @@ describe('skjema', () => {
   });
 
   it('skal ha en knapp for å gå videre til neste steg', () => {
-    render(<Introduksjon />);
+    renderWithStegContext(<Introduksjon />);
     const button = screen.getByRole('button', { name: 'Neste' });
     expect(button).toBeVisible();
   });
 
-  it('skal vise en feilmelding dersom man ikke bekrefter', async () => {
-    render(<Introduksjon />);
+  it.skip('skal vise en feilmelding dersom man ikke bekrefter', async () => {
+    renderWithStegContext(<Introduksjon />);
     const button = screen.getByRole('button', { name: 'Neste' });
     await user.click(button);
     const feilmelding = screen.getByText('Du må bekrefte at du vil fylle ut meldekortet så riktig du kan');
