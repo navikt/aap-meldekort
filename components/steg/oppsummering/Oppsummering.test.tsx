150b2e84829a46bb3001c6d1a0b2861fe5c21bd4
Øivind Stensrud <oivind.stensrud@nav.no>
2024-11-25 16:34:31 +0100

    :sparkles: Implementasjon av Oppsummering

--- components/steg/oppsummering/Oppsummering.test.tsx
+++ components/steg/oppsummering/Oppsummering.test.tsx
@@ -1,7 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { Oppsummering } from 'components/steg/oppsummering/Oppsummering';
 import { describe, expect, it } from 'vitest';
 
 describe('Oppsummering', () => {
-  it('skal gjøre et eller annet', () => {
-    expect(true).toBeTruthy();
+  it('viser en suksess-melding', () => {
+    render(<Oppsummering />);
+    expect(
+      screen.getByText('Meldekortet ditt er sendt til Nav, du får beskjed hvis vi trenger noe mer fra deg.')
+    ).toBeVisible();
+  });
+
+  it('har en accordion for å se hva som ble sendt inn', () => {
+    render(<Oppsummering />);
+    expect(screen.getByText('Se hva du sendte inn')).toBeVisible();
+  });
+
+  it('har en knapp som tar deg til "Mine aap"', () => {
+    render(<Oppsummering />);
+    expect(screen.getByRole('button', { name: 'Gå til Mine AAP' })).toBeVisible();
   });
 });
--- components/steg/oppsummering/Oppsummering.tsx
+++ components/steg/oppsummering/Oppsummering.tsx
@@ -1,3 +1,20 @@
+import { Accordion, Alert, Button } from '@navikt/ds-react';
+
 export const Oppsummering = () => {
-  return <div>Oppsummering</div>;
+  return (
+    <div>
+      <Alert variant="success">
+        Meldekortet ditt er sendt til Nav, du får beskjed hvis vi trenger noe mer fra deg.
+      </Alert>
+      <Accordion>
+        <Accordion.Item>
+          <Accordion.Header>Se hva du sendte inn</Accordion.Header>
+          <Accordion.Content>Her kommer det innhold</Accordion.Content>
+        </Accordion.Item>
+      </Accordion>
+      <Button variant="primary" type="button" as={'a'}>
+        Gå til Mine AAP
+      </Button>
+    </div>
+  );
 };
