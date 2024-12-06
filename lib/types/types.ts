01c169cd8302f6ee0edf123b155dfe811b64d76a
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-12-06 13:51:29 +0100

    :label: Typer generert fra backend

---
+++ lib/types/types.ts
@@ -0,0 +1,5 @@
+import { components } from 'lib/types/schema';
+
+type meldekortRequest = components['schemas']['no.nav.aap.meldekort.arena.MeldekortRequest'];
+type meldekortResponse = components['schemas']['no.nav.aap.meldekort.arena.MeldekortResponse'];
+type meldekort = components['schemas']['no.nav.aap.meldekort.arena.MeldekortDto'];
