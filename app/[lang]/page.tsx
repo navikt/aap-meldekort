1c165c7760dd0de44f2e6e0a82cd7e071edcab53
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2024-12-18 11:05:06 +0100

    :recycle: viser oversikt siden dersom den ikke finner meldeperioder

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -5,12 +5,9 @@ import { redirect } from 'next/navigation';
 export default async function Home() {
   const meldeperioder = await hentMeldeperiode();
 
-  if (!meldeperioder || meldeperioder.length === 0) {
-    return <div>Kunne ikke finne meldeperioder.</div>;
-  }
   const meldeperiode = meldeperioder
-    .sort((a, b) => new Date(a.periode.fom).getTime() - new Date(b.periode.fom).getTime())
-    .find((meldeperiode) => meldeperiode.type === 'ORDINÆRT' && meldeperiode.klarForInnsending);
+    ?.sort((a, b) => new Date(a.periode.fom).getTime() - new Date(b.periode.fom).getTime())
+    ?.find((meldeperiode) => meldeperiode.type === 'ORDINÆRT' && meldeperiode.klarForInnsending);
 
   if (meldeperiode) {
     redirect(`/${meldeperiode.meldekortId}`);
--- components/oversikt/Oversikt.tsx
+++ components/oversikt/Oversikt.tsx
@@ -4,10 +4,12 @@ import { Meldeperiode } from 'lib/types/types';
 import { Accordion, Alert, BodyShort, Heading } from '@navikt/ds-react';
 
 interface Props {
-  meldeperioder: Meldeperiode[];
+  meldeperioder?: Meldeperiode[];
 }
 
 export const Oversikt = ({ meldeperioder }: Props) => {
+  const meldeperioderSomKanEndres = meldeperioder?.filter((meldeperiode) => meldeperiode.kanEndres);
+
   return (
     <div className={'flex-column'}>
       <Alert variant={'info'}>Du har ingen meldekort som må sendes inn.</Alert>
@@ -20,18 +22,16 @@ export const Oversikt = ({ meldeperioder }: Props) => {
           Her kan du se alle tidligere meldekort du har sendt til Nav. Du kan endre et meldekort ved å klikke på
           oversikten under.
         </BodyShort>
-        {meldeperioder
-          .filter((meldeperiode) => meldeperiode.kanEndres)
-          .map((meldeperiode) => {
-            return (
-              <Accordion key={meldeperiode.meldekortId}>
-                <Accordion.Item>
-                  <Accordion.Header>{meldeperiode.meldekortId}</Accordion.Header>
-                  <Accordion.Content>{JSON.stringify(meldeperiode)}</Accordion.Content>
-                </Accordion.Item>
-              </Accordion>
-            );
-          })}
+        {meldeperioderSomKanEndres?.map((meldeperiode) => {
+          return (
+            <Accordion key={meldeperiode.meldekortId}>
+              <Accordion.Item>
+                <Accordion.Header>{meldeperiode.meldekortId}</Accordion.Header>
+                <Accordion.Content>{JSON.stringify(meldeperiode)}</Accordion.Content>
+              </Accordion.Item>
+            </Accordion>
+          );
+        })}
       </div>
     </div>
   );
