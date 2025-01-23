f0f225c5c79ddb0ccd00928130112e88e49442ab
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-01-23 21:33:33 +0100

    :recycle: Slår sammen linkpanel og LinkPanelMeldekort til en komponent

--- components/linkpanel/LinkPanel.tsx
+++ components/linkpanel/LinkPanel.tsx
@@ -1,19 +1,33 @@
-import styles from 'components/linkpanel/LinkPanel.module.css';
 import { ChevronRightIcon } from '@navikt/aksel-icons';
 import { BodyShort } from '@navikt/ds-react';
+
+import styles from 'components/linkpanel/LinkPanel.module.css';
+import { MeldekortStatus } from 'components/meldekortstatus/MeldekortStatus';
+import { Status } from 'lib/types/types';
 
 interface Props {
   title: string;
   href: string;
+  variant?: 'primary' | 'secondary';
+  description?: string;
+  status?: Status;
 }
 
-export const LinkPanel = ({ title, href }: Props) => {
+export const LinkPanel = ({ title, href, description, status, variant = 'primary' }: Props) => {
+  const className = variant === 'primary' ? styles.primary : styles.secondary;
+
   return (
-    <a href={href} className={styles.link}>
-      <BodyShort size={'large'} style={{ fontWeight: 'bold' }}>
-        {title}
-      </BodyShort>
-      <ChevronRightIcon fontSize={'1.6rem'} aria-hidden="true" />
+    <a href={href} className={`${styles.link} ${className}`}>
+      <div className={styles.top}>
+        <BodyShort size={'large'} style={{ fontWeight: 'bold' }}>
+          {title}
+        </BodyShort>
+        <div className={styles.topright}>
+          {status && <MeldekortStatus status={status} />}
+          <ChevronRightIcon fontSize={'1.6rem'} aria-hidden="true" />
+        </div>
+      </div>
+      <BodyShort>{description}</BodyShort>
     </a>
   );
 };
