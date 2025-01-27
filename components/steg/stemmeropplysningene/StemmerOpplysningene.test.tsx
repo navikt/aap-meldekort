973ed08ad66639e8e84ea1e89586cbfee9d033c0
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-01-27 14:13:13 +0100

    :white_check_mark: Test på steg stemmer opplysningene

--- components/steg/stemmeropplysningene/StemmerOpplysningene.test.tsx
+++ components/steg/stemmeropplysningene/StemmerOpplysningene.test.tsx
@@ -1,12 +1,13 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import { MeldekortResponse } from 'lib/types/types';
 import { userEvent } from '@testing-library/user-event';
 import { StemmerOpplysningene } from 'components/steg/stemmeropplysningene/StemmerOpplysningene';
 
-const meldekort: MeldekortResponse = {
+const meldekortUtenArbeid: MeldekortResponse = {
   meldekort: {
+    harDuJobbet: false,
     timerArbeidet: [
       { dato: '2024-11-18' },
       { dato: '2024-11-19' },
@@ -28,30 +29,57 @@ const meldekort: MeldekortResponse = {
   periode: { fom: '2024-11-04', tom: '2024-11-17' },
 };
 
-describe('Timer arbeidet', () => {
-  beforeEach(() => render(<StemmerOpplysningene referanse={'123'} meldekort={meldekort} />));
+const meldekortMedArbeid: MeldekortResponse = {
+  meldekort: {
+    harDuJobbet: true,
+    timerArbeidet: [
+      { dato: '2024-11-18', timer: 4 },
+      { dato: '2024-11-19' },
+      { dato: '2024-11-20' },
+      { dato: '2024-11-21', timer: 5 },
+      { dato: '2024-11-22' },
+      { dato: '2024-11-23' },
+      { dato: '2024-11-24' },
+      { dato: '2024-11-25' },
+      { dato: '2024-11-26' },
+      { dato: '2024-11-27' },
+      { dato: '2024-11-28' },
+      { dato: '2024-11-29' },
+      { dato: '2024-11-30' },
+      { dato: '2024-12-01' },
+    ],
+  },
+  steg: 'TIMER_ARBEIDET',
+  periode: { fom: '2024-11-04', tom: '2024-11-17' },
+};
 
+describe('Stemmer opplysningene', () => {
   it('skal ha en overskrift', () => {
+    render(<StemmerOpplysningene referanse={'123'} meldekort={meldekortUtenArbeid} />);
     const heading = screen.getByRole('heading', { name: 'Se over før du sender inn', level: 2 });
     expect(heading).toBeVisible();
   });
 
   it('skal ha warning som sier at meldekortet ikke er sendt inn ennå', () => {
+    render(<StemmerOpplysningene referanse={'123'} meldekort={meldekortUtenArbeid} />);
     const heading = screen.getByText('Meldekortet er ikke sendt inn ennå');
     expect(heading).toBeVisible();
   });
 
-  it('skal ha en oppsummeringskalender', () => {
+  it('skal ha en oppsummeringskalender dersom det er oppgitt arbeid', () => {
+    render(<StemmerOpplysningene referanse={'123'} meldekort={meldekortMedArbeid} />);
     const oppsummeringskalender = screen.getByText('Uke 45 - 46');
     expect(oppsummeringskalender).toBeVisible();
   });
 
   it('skal ha et felt for å bekrefte at opplysningene stemmer', () => {
+    render(<StemmerOpplysningene referanse={'123'} meldekort={meldekortUtenArbeid} />);
     const bekreftOption = screen.getByRole('checkbox', { name: 'Jeg bekrefter at disse opplysningene stemmer' });
     expect(bekreftOption).toBeVisible();
   });
 
   it('skal gi en feilmelding dersom feltet for å bekrefte at opplysningene stemmer ikke er huket av', async () => {
+    render(<StemmerOpplysningene referanse={'123'} meldekort={meldekortUtenArbeid} />);
     const user = userEvent.setup();
     const sendInnKnapp = screen.getByRole('button', { name: 'Send inn' });
 
