a7d1140c5ef24e9d6eeee29b5aa676a67c3744a0
Øivind Stensrud <oivind.stensrud@nav.no>
2025-02-07 12:38:33 +0100

    :wheelchair: Skjermleser leser full dato for kolonne
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>

--- components/oppsummeringkalender/oppsummeringukerad/OppsummeringUkeRad.tsx
+++ components/oppsummeringkalender/oppsummeringukerad/OppsummeringUkeRad.tsx
@@ -4,6 +4,7 @@ import { Dag } from 'components/oppsummeringkalender/OppsummeringKalender';
 
 import styles from './OppsummeringUkeRad.module.css';
 import { formaterUkedag } from 'components/rapporteringskalender/ukerapportering/UkeRapportering';
+import { nb } from 'date-fns/locale/nb';
 
 interface Props {
   felterIUken: Dag[];
@@ -11,12 +12,13 @@ interface Props {
 
 export const OppsummeringUkeRad = ({ felterIUken }: Props) => {
   return (
-    <div className={styles.rad}>
+    <div className={styles.rad} role="list">
       {felterIUken.map((field, index) => {
+        const datoLabel = format(new Date(field.dag), 'eeee do MMMM', { locale: nb });
         const dagINummer = format(new Date(field.dag), 'd');
         return (
-          <div className={styles.dag} key={index}>
-            <BodyShort size={'small'} aria-hidden weight={'semibold'}>
+          <div className={styles.dag} key={index} aria-label={`${datoLabel}`} role="listitem">
+            <BodyShort size={'small'} weight={'semibold'} aria-hidden>
               {formaterUkedag(field.dag.toString())} {dagINummer}.
             </BodyShort>
             <div className={`${styles.timer} ${field.timer > 0 && styles.harTimer}`}>
