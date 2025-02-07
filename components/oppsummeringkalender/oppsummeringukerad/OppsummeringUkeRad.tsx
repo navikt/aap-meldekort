ac59076655bd11378017af028b4b5de992df333d
Øivind Stensrud <oivind.stensrud@nav.no>
2025-02-07 13:34:39 +0100

    :wheelchair: Bedre label for skjermleser i oppsummering
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>

--- components/oppsummeringkalender/oppsummeringukerad/OppsummeringUkeRad.tsx
+++ components/oppsummeringkalender/oppsummeringukerad/OppsummeringUkeRad.tsx
@@ -12,17 +12,22 @@ interface Props {
 
 export const OppsummeringUkeRad = ({ felterIUken }: Props) => {
   return (
-    <div className={styles.rad} role="list">
+    <div className={styles.rad}>
       {felterIUken.map((field, index) => {
         const datoLabel = format(new Date(field.dag), 'eeee do MMMM', { locale: nb });
         const dagINummer = format(new Date(field.dag), 'd');
         return (
-          <div className={styles.dag} key={index} aria-label={`${datoLabel}`} role="listitem">
-            <BodyShort size={'small'} weight={'semibold'} aria-hidden>
-              {formaterUkedag(field.dag.toString())} {dagINummer}.
-            </BodyShort>
-            <div className={`${styles.timer} ${field.timer > 0 && styles.harTimer}`}>
-              <BodyShort>{`${field.timer}t`}</BodyShort>
+          <div className={styles.dag} key={index}>
+            <div className={styles.sronly}>
+              {datoLabel} {field.timer}t
+            </div>
+            <div aria-hidden>
+              <BodyShort size={'small'} weight={'semibold'} aria-hidden>
+                {formaterUkedag(field.dag.toString())} {dagINummer}.
+              </BodyShort>
+              <div className={`${styles.timer} ${field.timer > 0 && styles.harTimer}`}>
+                <BodyShort>{`${field.timer}t`}</BodyShort>
+              </div>
             </div>
           </div>
         );
