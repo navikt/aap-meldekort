020fab4c8555022fe0a261d5208635221e5f3d5b
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-02-10 16:26:38 +0100

    Legger til tre nye spørsmål fra det gamle meldekortet

--- components/flyt/innsending/steg/spørsmål/Spørsmål.tsx
+++ components/flyt/innsending/steg/spørsmål/Spørsmål.tsx
@@ -16,19 +16,49 @@ interface Props {
 }
 
 interface FormFields {
-  harArbeidet: string;
+  harDuJobbet: JaEllerNei;
+  harDuGjennomførtAvtaltAktivitetKursEllerUtdanning: JaEllerNei;
+  harDuVærtSyk: JaEllerNei;
+  harDuHattFerie: JaEllerNei;
 }
 
 export const SpRsmL = ({ meldekort, referanse }: Props) => {
   const { isLoading, løsStegOgGåTilNeste, errorMessage } = useLøsStegOgGåTilNesteSteg(referanse);
 
   const { form, formFields } = useConfigForm<FormFields>({
-    harArbeidet: {
+    harDuJobbet: {
       type: 'radio',
       options: JaEllerNeiOptions,
       defaultValue: getJaNeiEllerUndefined(meldekort.meldekort.harDuJobbet),
       label: 'Har du arbeidet i perioden?',
       rules: { required: 'Du må svare på om du har arbeidet i perioden' },
+    },
+    harDuVærtSyk: {
+      type: 'radio',
+      options: JaEllerNeiOptions,
+      defaultValue: getJaNeiEllerUndefined(meldekort.meldekort.harDuVærtSyk),
+      label: 'Har du vært forhindret fra å ta arbeid eller gjennomføre avtalt aktivitet fordi du har vært for syk?',
+      rules: {
+        required:
+          'Du må svare på om du har vært forhindret fra å ta arbeid eller gjennomføre avtalt aktivitet fordi du har vært for syk',
+      },
+    },
+    harDuHattFerie: {
+      type: 'radio',
+      options: JaEllerNeiOptions,
+      defaultValue: getJaNeiEllerUndefined(meldekort.meldekort.harDuHattFerie),
+      label: 'Har du hatt ferie eller fravær slik at du ikke har kunnet ta arbeid eller gjennomføre avtalt aktivitet?',
+      rules: {
+        required:
+          'Du må svar på om du har hatt ferie eller fravær slik at du ikke har kunnet ta arbeid eller gjennomføre avtalt aktivitet',
+      },
+    },
+    harDuGjennomførtAvtaltAktivitetKursEllerUtdanning: {
+      type: 'radio',
+      options: JaEllerNeiOptions,
+      defaultValue: getJaNeiEllerUndefined(meldekort.meldekort.harDuGjennomførtAvtaltAktivitetKursEllerUtdanning),
+      label: 'Har du gjennomført avtalt aktivitet eller deltatt på kurs/utdanning?',
+      rules: { required: 'Du må svare på om du har gjennomført avtalt aktivitet eller deltatt på kurs/utdanning?' },
     },
   });
 
@@ -44,11 +74,15 @@ export const SpRsmL = ({ meldekort, referanse }: Props) => {
         løsStegOgGåTilNeste({
           meldekort: {
             ...meldekort.meldekort,
-            harDuJobbet: data.harArbeidet === JaEllerNei.Ja,
+            harDuJobbet: data.harDuJobbet === JaEllerNei.Ja,
+            harDuGjennomførtAvtaltAktivitetKursEllerUtdanning:
+              data.harDuGjennomførtAvtaltAktivitetKursEllerUtdanning === JaEllerNei.Ja,
+            harDuVærtSyk: data.harDuVærtSyk === JaEllerNei.Ja,
+            harDuHattFerie: data.harDuHattFerie === JaEllerNei.Ja,
             dager: meldekort.meldekort.dager.map((dag) => {
               return {
                 dato: dag.dato,
-                timerArbeidet: data.harArbeidet === JaEllerNei.Nei ? 0 : dag.timerArbeidet,
+                timerArbeidet: data.harDuJobbet === JaEllerNei.Nei ? 0 : dag.timerArbeidet,
               };
             }),
           },
@@ -66,7 +100,14 @@ export const SpRsmL = ({ meldekort, referanse }: Props) => {
           </Heading>
           <BodyShort>{`${formaterDatoForFrontend(fraDato)} - ${formaterDatoForFrontend(tilDato)}`}</BodyShort>
         </div>
-        <FormField form={form} formField={formFields.harArbeidet} size={'medium'} />
+        <FormField form={form} formField={formFields.harDuJobbet} size={'medium'} />
+        <FormField
+          form={form}
+          formField={formFields.harDuGjennomførtAvtaltAktivitetKursEllerUtdanning}
+          size={'medium'}
+        />
+        <FormField form={form} formField={formFields.harDuVærtSyk} size={'medium'} />
+        <FormField form={form} formField={formFields.harDuHattFerie} size={'medium'} />
       </HGrid>
     </Form>
   );
