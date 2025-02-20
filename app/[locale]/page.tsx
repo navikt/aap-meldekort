66ef53097eb53b086ee1c687a3a37550f670669c
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-02-20 12:29:54 +0100

    Legger inn en redirect som fører til felles løsning for Arena vedtak

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,15 +6,20 @@ import {
 } from 'lib/services/meldekortservice';
 
 export default async function Page() {
-  const innsendteMeldeperioder = await hentInnsendteMeldeperioder();
-  const kommendeMeldeperiode = await hentKommendeMeldeperiode();
   const ansvarligSystem = await hentAnsvarligSystem();
 
-  console.log('innsendte meldeperioder', innsendteMeldeperioder);
-  console.log('kommende meldeperiode', kommendeMeldeperiode);
-  console.log('ansvarlig system', ansvarligSystem);
+  if (ansvarligSystem === 'FELLES') {
+    //TODO Før vi går i produksjon så må vi redirecte til korrekt meldekortløsning basert på svaret vi får fra ansvarligSystem
+    // redirect('https://www.nav.no/nav.no-ressurser/lenker/selvbetjening/tjenester-pa-nav.no/send-meldekort');
+  }
+
+  const innsendteMeldeperioder = await hentInnsendteMeldeperioder();
+  const kommendeMeldeperiode = await hentKommendeMeldeperiode();
 
   return (
-    <Oversikt kommendeMeldeperiode={kommendeMeldeperiode} harInnsendteMeldeperioder={innsendteMeldeperioder.length > 0} />
+    <Oversikt
+      kommendeMeldeperiode={kommendeMeldeperiode}
+      harInnsendteMeldeperioder={innsendteMeldeperioder.length > 0}
+    />
   );
 }
