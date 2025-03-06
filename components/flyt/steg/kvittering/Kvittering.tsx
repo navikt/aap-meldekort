ca7716a66501d88eec34f301c010288decbb3f03
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-06 09:49:54 +0100

    Oversettelser på kvittering siden

--- components/flyt/steg/kvittering/Kvittering.tsx
+++ components/flyt/steg/kvittering/Kvittering.tsx
@@ -7,6 +7,7 @@ import { Link } from 'i18n/routing';
 import styles from 'components/flyt/steg/kvittering/Kvittering.module.css';
 import { SkjemaOppsummering } from 'components/skjemaoppsummering/SkjemaOppsummering';
 import { InnsendingType, useParamsMedType } from 'lib/utils/url';
+import { useTranslations } from 'next-intl';
 
 interface Props {
   utfylling: UtfyllingResponse;
@@ -14,33 +15,40 @@ interface Props {
 }
 
 export const Kvittering = ({ utfylling }: Props) => {
+  const t = useTranslations();
   const { innsendingtype } = useParamsMedType();
 
   return (
     <VStack gap={'8'}>
       <Alert variant="success">
         {innsendingtype === InnsendingType.INNSENDING
-          ? 'Vi har mottatt meldekortet ditt.'
-          : 'Vi har mottatt endringene på meldekortet'}
+          ? t('client.steg.kvittering.alert.innsending.label')
+          : t('client.steg.kvittering.alert.korrigering.label')}
       </Alert>
 
       <List>
-        <List.Item>Du kan endre opplysningene hvis du oppdager at du har ført feil.</List.Item>
-        <List.Item>Du vil få utbetalt AAP om cirka 2 til 3 virkerdager.</List.Item>
+        <List.Item>{t('client.steg.kvittering.bulletList.items.1')}</List.Item>
+        <List.Item>{t('client.steg.kvittering.bulletList.items.2')}</List.Item>
       </List>
 
       <Accordion>
         <Accordion.Item>
-          <Accordion.Header>Se hva du sendte inn</Accordion.Header>
+          <Accordion.Header>{t('client.steg.kvittering.sehvadusendteinn')}</Accordion.Header>
           <Accordion.Content>
             <SkjemaOppsummering utfylling={utfylling} />
           </Accordion.Content>
         </Accordion.Item>
       </Accordion>
 
-      <Link href={`/`} className={styles.link}>
-        Gå tilbake til oversikt
-      </Link>
+      {t.rich('client.steg.kvittering.link', {
+        a: (chunks) => {
+          return (
+            <Link href={`/`} className={styles.link}>
+              {chunks}
+            </Link>
+          );
+        },
+      })}
     </VStack>
   );
 };
