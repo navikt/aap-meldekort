3a40daad94cec984ca2254fb57dd552a0ff3a113
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-03-25 16:08:20 +0100

    Responsivt design innsendte meldekort

--- components/sider/innsendtemeldekort/InnsendteMeldekort.tsx
+++ components/sider/innsendtemeldekort/InnsendteMeldekort.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { Alert, BodyLong, Heading, VStack } from '@navikt/ds-react';
+import { Alert, BodyLong, BodyShort, Heading, HStack, VStack } from '@navikt/ds-react';
 import { formaterDatoMedÅrForFrontend, hentUkeNummerForPeriode } from 'lib/utils/date';
-import { NavigationPanel } from 'components/navigationpanel/NavigationPanel';
 import { MeldekortLenke } from 'components/meldekortlenke/MeldekortLenke';
 import { HistoriskMeldeperiode } from 'lib/types/types';
 import { PencilIcon } from '@navikt/aksel-icons';
@@ -10,6 +9,9 @@ import { startKorrigeringClient } from 'lib/client/clientApi';
 import { useRouter } from 'i18n/routing';
 import { InnsendingType } from 'lib/utils/url';
 import { useTranslations } from 'next-intl';
+import { useSkjermBredde } from 'hooks/skjermbreddeHook';
+
+import styles from 'components/navigationpanel/NavigationPanel.module.css';
 
 interface Props {
   innsendteMeldeperioder: HistoriskMeldeperiode[];
@@ -18,6 +20,7 @@ interface Props {
 export const InnsendteMeldekort = ({ innsendteMeldeperioder }: Props) => {
   const t = useTranslations();
   const router = useRouter();
+  const { erLitenSkjerm } = useSkjermBredde();
 
   return (
     <VStack gap={'8'}>
@@ -34,9 +37,7 @@ export const InnsendteMeldekort = ({ innsendteMeldeperioder }: Props) => {
           <>
             {innsendteMeldeperioder.map((innsendtMeldekort, key) => {
               return (
-                <NavigationPanel
-                  key={key}
-                  type={'button'}
+                <button
                   onClick={async () => {
                     const startInnsendingAvMeldekortResponse = await startKorrigeringClient(
                       innsendtMeldekort.meldeperiode
@@ -48,18 +49,38 @@ export const InnsendteMeldekort = ({ innsendteMeldeperioder }: Props) => {
                       );
                     }
                   }}
-                  rightIcon={<PencilIcon aria-hidden="true" fontSize={'1.5rem'} />}
-                  title={t('client.innsendteMeldekort.innsendtMeldekort.title', {
-                    uker: hentUkeNummerForPeriode(
-                      new Date(innsendtMeldekort.meldeperiode.fom),
-                      new Date(innsendtMeldekort.meldeperiode.tom)
-                    ),
-                    datoperiode: `${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.fom)} - ${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.tom)}`,
-                  })}
-                  description={t('client.innsendteMeldekort.innsendtMeldekort.description', {
-                    timer: innsendtMeldekort.antallTimerArbeidetIPerioden,
-                  })}
-                />
+                  key={key}
+                  className={styles.link}
+                >
+                  <HStack justify={'space-between'} align={'center'}>
+                    <HStack align={'center'} gap={'0 4'}>
+                      <VStack align={'start'} gap={'1'}>
+                        <BodyShort size={'large'} weight={'semibold'}>
+                          {t('client.innsendteMeldekort.innsendtMeldekort.title', {
+                            uker: hentUkeNummerForPeriode(
+                              new Date(innsendtMeldekort.meldeperiode.fom),
+                              new Date(innsendtMeldekort.meldeperiode.tom)
+                            ),
+                          })}
+                          {!erLitenSkjerm &&
+                            ` (${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.fom)} - ${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.tom)})`}
+                        </BodyShort>
+                        {erLitenSkjerm && (
+                          <BodyShort
+                            size={'large'}
+                          >{`${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.fom)} - ${formaterDatoMedÅrForFrontend(innsendtMeldekort.meldeperiode.tom)}`}</BodyShort>
+                        )}
+
+                        <BodyShort size={'large'}>
+                          {t('client.innsendteMeldekort.innsendtMeldekort.description', {
+                            timer: innsendtMeldekort.antallTimerArbeidetIPerioden,
+                          })}
+                        </BodyShort>
+                      </VStack>
+                    </HStack>
+                    <PencilIcon aria-hidden="true" fontSize={'1.5rem'} />
+                  </HStack>
+                </button>
               );
             })}
           </>
