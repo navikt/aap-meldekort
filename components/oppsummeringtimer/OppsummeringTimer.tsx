663d6ae9261b6e8ff28a44d70182a336a7fb7e65
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2025-04-11 10:33:02 +0200

    Viser ikke prosent

--- components/oppsummeringtimer/OppsummeringTimer.tsx
+++ components/oppsummeringtimer/OppsummeringTimer.tsx
@@ -1,5 +1,4 @@
 import { OppsummeringRad } from 'components/oppsummeringrad/OppsummeringRad';
-import { regnUtProsent } from 'lib/utils/meldekort';
 import { useTranslations } from 'next-intl';
 
 interface Props {
@@ -8,12 +7,6 @@ interface Props {
 
 export const OppsummeringTimer = ({ timer }: Props) => {
   const t = useTranslations();
-  const antallTimerIProsent = regnUtProsent(timer);
 
-  return (
-    <OppsummeringRad
-      label={t('client.sammenlagtForPerioden.label')}
-      value={`${timer} timer (${antallTimerIProsent}%)`}
-    />
-  );
+  return <OppsummeringRad label={t('client.sammenlagtForPerioden.label')} value={`${timer} timer`} />;
 };
