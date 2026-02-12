86c7c933e07fa34e491fe0db5da48c2ecdc33b3a
Lars Grini <lars.strande.grini@nav.no>
2026-02-12 08:49:45 +0100

    new Reponse -> NextResponse

--- app/api/[meldekortid]/slett/route.ts
+++ app/api/[meldekortid]/slett/route.ts
@@ -1,17 +1,15 @@
-import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import { slettUtfylling } from 'lib/services/meldekortservice';
 import { logError } from '@navikt/aap-felles-utils';
 
-export async function DELETE(req: NextRequest, props: { params: Promise<{ meldekortid: string }> }) {
+export async function DELETE(props: { params: Promise<{ meldekortid: string }> }) {
   const params = await props.params;
 
   try {
     await slettUtfylling(params.meldekortid);
-    return new Response(null, { status: 204 });
+    return new NextResponse(null, { status: 204 });
   } catch (err) {
     logError(`/api/${params.meldekortid}/slett`, err);
-    return new Response(JSON.stringify({ message: 'Noe gikk galt i sletting av mellomlagring av utfylling' }), {
-      status: 500,
-    });
+    return NextResponse.json({ message: 'Noe gikk galt i sletting av mellomlagring av utfylling' }, { status: 500 });
   }
 }
