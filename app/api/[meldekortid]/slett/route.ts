f6e5c38d6ee5eb4117335ec1df30299b7bc43084
Øivind Stensrud <oivind.stensrud@nav.no>
2026-02-20 11:02:45 +0100

    Kall til DELETE på et meldekort ga feilmelding
    Manglet NextRequest som første parameter til route.
    
    Co-authored-by: Tor Idland <tor.idland@nav.no>
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>

--- app/api/[meldekortid]/slett/route.ts
+++ app/api/[meldekortid]/slett/route.ts
@@ -1,15 +1,19 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { slettUtfylling } from 'lib/services/meldekortservice';
 import { logError } from '@navikt/aap-felles-utils';
 
-export async function DELETE(props: { params: Promise<{ meldekortid: string }> }) {
+export async function DELETE(_: NextRequest, props: { params: Promise<{ meldekortid: string }> }) {
   const params = await props.params;
-
+  const meldekortid = params?.meldekortid;
+  if (!meldekortid) {
+    logError('MeldekortId finnes ikke ved kall til sletting av påbegynt meldekort, route /api/[meldekortid]/slett');
+    return NextResponse.json({ message: 'Noe gikk galt i sletting av mellomlagring av utfylling' }, { status: 400 });
+  }
   try {
-    await slettUtfylling(params.meldekortid);
+    await slettUtfylling(meldekortid);
     return new NextResponse(null, { status: 204 });
   } catch (err) {
-    logError(`/api/${params.meldekortid}/slett`, err);
+    logError(`/api/${meldekortid}/slett`, err);
     return NextResponse.json({ message: 'Noe gikk galt i sletting av mellomlagring av utfylling' }, { status: 500 });
   }
 }
