7f64a54319f563b0e1eb961dac522054e0f80d42
Øivind Stensrud <93574268+steoiv@users.noreply.github.com>
2026-09-08 11:51:48 +0200

    Oppgraderer vitest til 5.0.0 (#494)
    AAP-BAU

--- vitestSetup.tsx
+++ vitestSetup.tsx
@@ -1,4 +1,4 @@
-import { afterEach, beforeAll, vi } from 'vitest';
+import { afterEach, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 
@@ -9,28 +9,26 @@ fetchMocker.enableMocks();
 
 fetchMocker.mockResponse({ status: 200 });
 
-beforeAll(() => {
-  vi.mock('next/navigation', () => ({
-    useParams: vi.fn().mockReturnValue({ referanse: '123', innsendingtype: 'innsending' }),
-    useRouter: vi.fn().mockReturnValue({ prefetch: () => null }),
-  }));
+vi.mock('next/navigation', () => ({
+  useParams: vi.fn().mockReturnValue({ referanse: '123', innsendingtype: 'innsending' }),
+  useRouter: vi.fn().mockReturnValue({ prefetch: () => null }),
+}));
 
-  vi.mock('i18n/routing', () => ({
-    // Mocker opp Link til å returnere en a tag slik at vi får korrekt rolle i tester
-    Link: vi.fn().mockImplementation(({ href, children, ...props }) => (
-      <a href={href} {...props}>
-        {children}
-      </a>
-    )),
-    redirect: vi.fn(),
-    usePathname: vi.fn().mockReturnValue('/sett-inn-riktig-value-her-hvis-det-trengs-en-gang-i-fremtiden'),
-    useRouter: vi.fn().mockReturnValue({
-      push: vi.fn(),
-      replace: vi.fn(),
-      prefetch: vi.fn(),
-    }),
-  }));
-});
+vi.mock('i18n/routing', () => ({
+  // Mocker opp Link til å returnere en a tag slik at vi får korrekt rolle i tester
+  Link: vi.fn().mockImplementation(({ href, children, ...props }) => (
+    <a href={href} {...props}>
+      {children}
+    </a>
+  )),
+  redirect: vi.fn(),
+  usePathname: vi.fn().mockReturnValue('/sett-inn-riktig-value-her-hvis-det-trengs-en-gang-i-fremtiden'),
+  useRouter: vi.fn().mockReturnValue({
+    push: vi.fn(),
+    replace: vi.fn(),
+    prefetch: vi.fn(),
+  }),
+}));
 
 afterEach(() => {
   cleanup();
